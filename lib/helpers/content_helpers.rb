4e62784c6120cbc172aabe1716e513fb79001548
Chris Woodford <chris.woodford@gmail.com>
2019-10-24 17:06:02 -0400

    Clean up the Content Tag Helpers

--- lib/helpers/content_helpers.rb
+++ lib/helpers/content_helpers.rb
@@ -1,5 +1,5 @@
 module ContentHelpers
-  def article(&block)
+  def article_tag(&block)
     classes = %w(w-full px-4 md:px-6 text-xl text-gray-800 leading-normal)
     content_tag(:article, class: classes.join(" "), &block)
   end
@@ -14,12 +14,23 @@ module ContentHelpers
     content_tag(:h1, text, class: classes.join(" "))
   end
 
-  def article_section(&block)
+  def section_tag(&block)
     body = capture_html(&block)
     content_tag(:section, body, class: "py-4")
   end
 
-  def article_paragraph(&block)
+  def section_title(text)
+    classes = %w(font-bold font-sans break-normal text-gray-900 text-2xl
+                 md:text-3xl)
+    content_tag(:h2, text, class: classes.join(" "))
+  end
+
+  def paragraph_tag(&block)
     content_tag(:p, class: "pb-4", &block)
+  end
+
+  def blockquote_tag(cite:, &block)
+    classes = %w(border-l-4 border-indigo-500 italic my-8 pl-8 md:pl-12)
+    content_tag(:blockquote, cite: cite, class: classes.join(" "), &block)
   end
 end
